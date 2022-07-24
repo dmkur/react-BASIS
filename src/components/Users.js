978517deae3046290b49345e1f79baa15f3412a6
dmkur <fgameua@gmail.com>
2022-07-24 10:45:27 +0300

    06-lesson part 2 class component lifeCycle

--- src/components/Users.js
+++ src/components/Users.js
@@ -3,16 +3,26 @@ import {ApiUsersServices} from "../services/api.users.services";
 import User from "./User";
 
 class Users extends Component {
+    // useState
     state = {users:[], x:'lol'}
 
     constructor(props, context) {
+        console.log('constructor')
         super(props, context);
+
+    }
+
+    // useEffect
+    componentDidMount() {
+        console.log('componentDidMount')
         this.apiUserServices = new ApiUsersServices()
         // this.apiUserServices.getUsers().then(value => this.setState({...this.state, users:value}))
         this.apiUserServices.getUsers().then(value => this.setState({users:value}))
     }
 
     render() {
+        // return
+        console.log('render')
         return (
             <div>
                 <h2>Users Component</h2>
