5cb37926b3c24aa76ccda7f78affd90778626b9b
dmkur <fgameua@gmail.com>
2022-07-28 21:49:59 +0300

    07-lesson redux toolkit

--- src/redux/slices/counter1.slice.js
+++ src/redux/slices/counter1.slice.js
@@ -2,14 +2,25 @@ import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
 
 let initialState = {
     count1: 0,
-    users: []
+    users: [],
+    isLoading: false,
+    serverError: null
 };
 
 const getAllUsers = createAsyncThunk(
     'count1Slice/getAllUsers',
-    async () => {
-        return await fetch('https://jsonplaceholder.typicode.com/users')
-            .then(value => value.json())
+    async (_, {rejectWithValue}) => {
+        try {
+            return await fetch('https://jsonplaceholder.typicode.com/users')
+                .then(response => {
+                    if (!response.ok) {
+                        throw Error('Bad Request')
+                    }
+                    return response.json()
+                })
+        } catch (e) {
+            return rejectWithValue(e.message)
+        }
 
     }
 );
@@ -28,11 +39,35 @@ const count1Slice = createSlice({
             state.count1 = 0
         }
     },
-    extraReducers:{
-        [getAllUsers.fulfilled]:(state, action) => {
-            state.users = action.payload
-        }
+    //method 1
+    // extraReducers: {
+    //     [getAllUsers.fulfilled]: (state, action) => {
+    //         state.isLoading = false
+    //         state.users = action.payload
+    //     },
+    //     [getAllUsers.pending]: (state) => {
+    //         state.isLoading = true
+    //
+    //     },
+    //     [getAllUsers.rejected]: (state, action) => {
+    //         state.serverError = action.payload
+    //     }
+    // }
+    //method 2
+    extraReducers: (builder) => {
+        builder
+            .addCase(getAllUsers.fulfilled, (state, action) => {
+                state.isLoading = false
+                state.users = action.payload
+            })
+            .addCase(getAllUsers.pending, state => {
+                state.isLoading = true
+            })
+            .addCase(getAllUsers.rejected, (state, action) => {
+                state.serverError = action.payload
+            })
     }
+
 });
 
 const {reducer: count1Reducer, actions: {inc, dec, reset}} = count1Slice;
